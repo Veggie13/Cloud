63e3c45d7c09ea54326102373e27b6731f3f0356
Veggie <cmderochie+github@gmail.com>
2013-08-01 22:32:51 -0600

    Initial commit.

---
+++ Nebula.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FlexDraw;
+
+namespace Cloud
+{
+    class Nebula : IDrawable
+    {
+        private const double G = 1;
+
+        public void Draw(IDrawAPI api)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Visible
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public PointD Origin
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public RectangleD Bounds
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public RectangleD LastBounds
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event DrawableModifiedEvent Modified;
+
+        private class Particle
+        {
+            public PointD Pos = new PointD();
+            public double Mass = 0.0;
+        }
+    }
+}
